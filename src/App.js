01fdc01fe8090be30cf1e2114ca6041243ea9e73
MikeKowalsky <mkowalewski83@gmail.com>
2018-06-28 15:39:06 +0200

    SecDay till first assignment

--- src/App.js
+++ src/App.js
@@ -3,12 +3,68 @@ import './App.css';
 import Person from './Person/Person'
 
 class App extends Component {
+
+  state = {
+    persons: [
+      {name: 'Mike', age: 34},
+      {name: 'John', age: 33},
+      {name: 'Luke', age: 22}
+    ]
+  }
+
+  switchNameHandler = (newName) => {
+    // console.log('Was clicked!!');
+    this.setState({
+      persons: [
+        {name: newName, age: 34},
+        {name: 'John', age: 33},
+        {name: 'Luke', age: 18}
+      ]
+    })
+  }
+
+  changeNameHandler = (event) => {
+    this.setState({
+      persons: [
+        {name: 'Mike', age: 34},
+        {name: event.target.value, age: 33},
+        {name: 'Luke', age: 22}
+      ]
+    })
+  }
+
+
+  // w button  ->  second way to pass argument to a function, but this way can be inefficient
   render() {
+
+    const myStyle = {
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer'
+    }
+
     return (
       <div className="App">
         <h1>Hi, I'm a React App.</h1>
         <p>This is really working!</p>
-        <Person />
+        
+        <button 
+          style={myStyle}
+          onClick = {() => this.switchNameHandler('Michael')}>Switch Name</button>
+        <Person 
+          name={this.state.persons[0].name} 
+          age={this.state.persons[0].age}/>
+        <Person 
+          name={this.state.persons[1].name} 
+          age={this.state.persons[1].age}
+          // first way to pass arguments, when we are passing a function as a prop
+          click={this.switchNameHandler.bind(this, 'Mike!!!')}
+          changed={this.changeNameHandler}>My Hobbies: Racing</Person>
+        <Person 
+          name={this.state.persons[2].name} 
+          age={this.state.persons[2].age}/>
       </div>
     );
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'I\'m a React App!!!!'));
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,7 +1,19 @@
 import React from 'react';
 
-const person = () => {
-	return <p>I'm a person.</p>;
+import './Person.css';
+
+// in input -> binding is in both ways, value change the state, 
+// we listen the changes - onChange we update the state, 
+// we can see in input the value from the beginning 	
+
+const person = (props) => {
+	return (
+		<div className="Person">
+			<p onClick = {props.click}>I'm {props.name} and I'm {props.age} years old.</p>
+			<p>{props.children}</p>
+			<input type="text" onChange={props.changed} value={props.name}/>
+		</div>
+	)
 }
 
 export default person;
